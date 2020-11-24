edaf4229c613cdc415192d0fb681597d6dc9834d
kiba0510 <kiba0510@gmail.com>
2020-11-24 19:23:07 +0000

    added header file

---
+++ 0_principal_header.h
@@ -0,0 +1,22 @@
+#ifmdef SHELL_H
+#define SHELL_H
+
+/*-----Libraries-----*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+#include <dirent.h>
+#include <limits.h>
+
+/*-----Headers-----*/
+#include 1_path_func.h
+
+/*-----print enviroment variables-----*/
+extern char **environt
+
+#endif
